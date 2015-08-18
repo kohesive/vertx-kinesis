c30b5b9071612b5d435d819135c54408b4c2d8d3
Alexander Eliseyev <a.a.eliseyev@gmail.com>
2015-08-18 19:07:10 +0300

    AsyncHandler refactoring

--- src/main/kotlin/org/collokia/vertx/kinesis/KinesisStreamConsumerVerticle.kt
+++ src/main/kotlin/org/collokia/vertx/kinesis/KinesisStreamConsumerVerticle.kt
@@ -1,10 +1,9 @@
 package org.collokia.vertx.kinesis
 
 import com.collokia.vertx.util.getFromSharedMemoryAsync
-import io.vertx.core.AsyncResult
+import com.collokia.vertx.util.onSuccess
 import io.vertx.core.DeploymentOptions
 import io.vertx.core.Future
-import io.vertx.core.Handler
 import io.vertx.core.json.JsonObject
 import io.vertx.core.logging.LoggerFactory
 import java.util.concurrent.CopyOnWriteArrayList
@@ -20,46 +19,40 @@ class KinesisStreamConsumerVerticle : KinesisVerticle() {
     private var shardVerticlesDeploymentIds = CopyOnWriteArrayList<String>()
 
     override fun startAfterClientInit(startFuture: Future<Void>) {
-        vertxClient.describeStream(getStreamName(), null, null) {
-            if (it.succeeded()) {
-                val shardIds = it.result().getJsonArray("shards").map { (it as? JsonObject)?.getString("shardId") }.filterNotNull()
-                val latch = CountDownLatch(shardIds.size())
+        vertxClient.describeStream(getStreamName(), null, null, onSuccess<JsonObject> {
+            val shardIds = it.getJsonArray("shards").map { (it as? JsonObject)?.getString("shardId") }.filterNotNull()
+            val latch = CountDownLatch(shardIds.size())
 
-                for (shardId in shardIds) {
-                    vertx.getFromSharedMemoryAsync(KinesisVerticle.ShardIteratorMapName, getShardIteratorKey(shardId), Handler { iteratorAsync: AsyncResult<String?> ->
-                        if (iteratorAsync.succeeded()) {
-                            val shardVerticleConfig = config().copy()
-                                .put("shardId", shardId)
-                                .put("shardIterator", iteratorAsync.result())
+            for (shardId in shardIds) {
+                vertx.getFromSharedMemoryAsync(KinesisVerticle.ShardIteratorMapName, getShardIteratorKey(shardId), onSuccess<String?> {
+                        val shardVerticleConfig = config().copy()
+                            .put("shardId", shardId)
+                            .put("shardIterator", it)
 
-                            vertx.deployVerticle(
-                                config().getString("shardConsumerVerticleName"),
-                                DeploymentOptions().setConfig(shardVerticleConfig),
-                                Handler {
-                                    if (it.succeeded()) {
-                                        shardVerticlesDeploymentIds.add(it.result())
-                                        latch.countDown()
-                                    } else {
-                                        log.error("Can't start shard consumer verticle", it.cause())
-                                    }
-                                }
-                            )
-                        } else {
-                            log.error("Can't retrieve shard iterator from shared memory", it.cause())
-                        }
+                        vertx.deployVerticle(
+                            config().getString("shardConsumerVerticleName"),
+                            DeploymentOptions().setConfig(shardVerticleConfig),
+                            onSuccess<String> {
+                                shardVerticlesDeploymentIds.add(it)
+                                latch.countDown()
+                            } onFail {
+                                log.error("Can't start shard consumer verticle", it)
+                            }
+                        )
+                    } onFail {
+                        log.error("Can't retrieve shard iterator from shared memory", it)
                     })
-                }
-
-                if (latch.await(10, TimeUnit.SECONDS)) {
-                    log.info("Deployed ${ shardIds.size() } shard consumer verticles")
-                    startFuture.complete()
-                } else {
-                    startFuture.fail("Can't initialize shard consumers")
-                }
-            } else {
-                startFuture.fail(it.cause())
             }
-        }
+
+            if (latch.await(10, TimeUnit.SECONDS)) {
+                log.info("Deployed ${ shardIds.size() } shard consumer verticles")
+                startFuture.complete()
+            } else {
+                startFuture.fail("Can't initialize shard consumers")
+            }
+        } onFail {
+            startFuture.fail(it)
+        })
     }
 
     override fun stopAfterClientDispose(stopFuture: Future<Void>) {
@@ -67,13 +60,11 @@ class KinesisStreamConsumerVerticle : KinesisVerticle() {
         val latch = CountDownLatch(shardVerticlesDeploymentIds.size())
 
         shardVerticlesDeploymentIds.forEach { deploymentId ->
-            vertx.undeploy(deploymentId, Handler {
-                if (it.succeeded()) {
-                    shardVerticlesDeploymentIds.remove(deploymentId)
-                    latch.countDown()
-                } else {
-                    log.error("Can't stop shard consumer verticle", it.cause())
-                }
+            vertx.undeploy(deploymentId, onSuccess<Void> {
+                shardVerticlesDeploymentIds.remove(deploymentId)
+                latch.countDown()
+            } onFail {
+                log.error("Can't stop shard consumer verticle", it)
             })
         }
 
